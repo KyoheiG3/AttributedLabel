57b033d0061bc722e2a95415870d2c3ec2900163
ito_kyohei <ito_kyohei@cyberagent.co.jp>
2015-07-17 03:25:52 +0900

    add project

---
+++ AttributedLabel/AttributedLabel.h
@@ -0,0 +1,19 @@
+//
+//  AttributedLabel.h
+//  AttributedLabel
+//
+//  Created by Kyohei Ito on 2015/07/17.
+//  Copyright © 2015年 Kyohei Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AttributedLabel.
+FOUNDATION_EXPORT double AttributedLabelVersionNumber;
+
+//! Project version string for AttributedLabel.
+FOUNDATION_EXPORT const unsigned char AttributedLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttributedLabel/PublicHeader.h>
+
+
---
+++ AttributedLabel/AttributedLabel.swift
@@ -0,0 +1,192 @@
+//
+//  AttributedLabel.swift
+//  AttributedLabel
+//
+//  Created by Kyohei Ito on 2015/07/17.
+//  Copyright © 2015年 Kyohei Ito. All rights reserved.
+//
+
+import UIKit
+
+class AttributedLabel: UIView {
+    enum ContentAlignment: Int {
+        case Center
+        case Top
+        case Bottom
+        case Left
+        case Right
+        case TopLeft
+        case TopRight
+        case BottomLeft
+        case BottomRight
+        
+        func alignOffset(viewSize viewSize: CGSize, containerSize: CGSize) -> CGPoint {
+            let xMargin = viewSize.width - containerSize.width
+            let yMargin = viewSize.height - containerSize.height
+            
+            switch self {
+            case Center:
+                return CGPoint(x: max(xMargin / 2, 0), y: max(yMargin / 2, 0))
+            case Top:
+                return CGPoint(x: max(xMargin / 2, 0), y: 0)
+            case Bottom:
+                return CGPoint(x: max(xMargin / 2, 0), y: max(yMargin, 0))
+            case Left:
+                return CGPoint(x: 0, y: max(yMargin / 2, 0))
+            case Right:
+                return CGPoint(x: max(xMargin, 0), y: max(yMargin / 2, 0))
+            case TopLeft:
+                return CGPoint(x: 0, y: 0)
+            case TopRight:
+                return CGPoint(x: max(xMargin, 0), y: 0)
+            case BottomLeft:
+                return CGPoint(x: 0, y: max(yMargin, 0))
+            case BottomRight:
+                return CGPoint(x: max(xMargin, 0), y: max(yMargin, 0))
+            }
+        }
+    }
+    
+    /// default is `0`.
+    var numberOfLines: Int = 0 {
+        didSet { setNeedsDisplay() }
+    }
+    /// default is `Left`.
+    var contentAlignment: ContentAlignment = .Left {
+        didSet { setNeedsDisplay() }
+    }
+    /// default is system font 17 plain.
+    var font = UIFont.systemFontOfSize(17) {
+        didSet { setNeedsDisplay() }
+    }
+    /// default is `ByTruncatingTail`.
+    var lineBreakMode: NSLineBreakMode = .ByTruncatingTail {
+        didSet { setNeedsDisplay() }
+    }
+    /// default is nil (text draws black).
+    var textColor: UIColor? {
+        didSet { setNeedsDisplay() }
+    }
+    /// default is nil.
+    var paragraphStyle: NSParagraphStyle? {
+        didSet { setNeedsDisplay() }
+    }
+    /// default is nil.
+    var shadow: NSShadow? {
+        didSet { setNeedsDisplay() }
+    }
+    /// default is nil.
+    var attributedText: NSAttributedString? {
+        didSet { setNeedsDisplay() }
+    }
+    /// default is nil.
+    var text: String? {
+        get {
+            return attributedText?.string
+        }
+        set {
+            if let value = newValue {
+                attributedText = NSAttributedString(string: value)
+            } else {
+                attributedText = nil
+            }
+        }
+    }
+    
+    private var mergedAttributedText: NSAttributedString? {
+        if let attributedText = attributedText {
+            return mergeAttributes(attributedText)
+        }
+        return nil
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        opaque = false
+        contentMode = .Redraw
+    }
+    
+    override func drawRect(rect: CGRect) {
+        guard let attributedText = mergedAttributedText else {
+            return
+        }
+        
+        let container = textContainer(rect.size)
+        let manager = layoutManager(container)
+        
+        let storage = NSTextStorage(attributedString: attributedText)
+        storage.addLayoutManager(manager)
+        
+        let frame = manager.usedRectForTextContainer(container)
+        let point = contentAlignment.alignOffset(viewSize: rect.size, containerSize: CGRectIntegral(frame).size)
+        
+        let glyphRange = manager.glyphRangeForTextContainer(container)
+        manager.drawBackgroundForGlyphRange(glyphRange, atPoint: point)
+        manager.drawGlyphsForGlyphRange(glyphRange, atPoint: point)
+    }
+    
+    override func sizeThatFits(size: CGSize) -> CGSize {
+        guard let attributedText = mergedAttributedText else {
+            return super.sizeThatFits(size)
+        }
+        
+        let container = textContainer(size)
+        let manager = layoutManager(container)
+        
+        let storage = NSTextStorage(attributedString: attributedText)
+        storage.addLayoutManager(manager)
+        
+        let frame = manager.usedRectForTextContainer(container)
+        return CGRectIntegral(frame).size
+    }
+    
+    override func sizeToFit() {
+        super.sizeToFit()
+        
+        frame.size = sizeThatFits(CGSize(width: bounds.width, height: CGFloat.max))
+    }
+    
+    private func textContainer(size: CGSize) -> NSTextContainer {
+        let container = NSTextContainer(size: size)
+        container.lineBreakMode = lineBreakMode
+        container.lineFragmentPadding = 0
+        container.maximumNumberOfLines = numberOfLines
+        return container
+    }
+    
+    private func layoutManager(container: NSTextContainer) -> NSLayoutManager {
+        let layoutManager = NSLayoutManager()
+        layoutManager.addTextContainer(container)
+        return layoutManager
+    }
+    
+    private func mergeAttributes(attributedText: NSAttributedString) -> NSAttributedString {
+        let attrString = NSMutableAttributedString(attributedString: attributedText)
+        
+        addAttribute(attrString, attrName: NSFontAttributeName, attr: font)
+        
+        if let textColor = textColor {
+            addAttribute(attrString, attrName: NSForegroundColorAttributeName, attr: textColor)
+        }
+        
+        if let paragraphStyle = paragraphStyle {
+            addAttribute(attrString, attrName: NSParagraphStyleAttributeName, attr: paragraphStyle)
+        }
+        
+        if let shadow = shadow {
+            addAttribute(attrString, attrName: NSShadowAttributeName, attr: shadow)
+        }
+        
+        return attrString
+    }
+    
+    private func addAttribute(attrString: NSMutableAttributedString, attrName: String, attr: AnyObject) {
+        let range = NSRange(location: 0, length: attrString.length)
+        attrString.enumerateAttribute(attrName, inRange: range, options: .Reverse) { object, range, pointer in
+            if object == nil {
+                attrString.addAttributes([attrName: attr], range: range)
+            }
+        }
+    }
+}
