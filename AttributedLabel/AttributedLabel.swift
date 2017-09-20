eb797b7ed0ee8df9b353bb19252e327eb1626710
ito_kyohei <ito_kyohei@cyberagent.co.jp>
2017-09-20 20:06:04 +0900

    Remove white spaces

--- AttributedLabel/AttributedLabel.swift
+++ AttributedLabel/AttributedLabel.swift
@@ -20,11 +20,11 @@ open class AttributedLabel: UIView {
         case topRight
         case bottomLeft
         case bottomRight
-        
+
         func alignOffset(viewSize: CGSize, containerSize: CGSize) -> CGPoint {
             let xMargin = viewSize.width - containerSize.width
             let yMargin = viewSize.height - containerSize.height
-            
+
             switch self {
             case .center:
                 return CGPoint(x: max(xMargin / 2, 0), y: max(yMargin / 2, 0))
@@ -47,7 +47,7 @@ open class AttributedLabel: UIView {
             }
         }
     }
-    
+
     /// default is `0`.
     @IBInspectable
     open var numberOfLines: Int {
@@ -113,46 +113,46 @@ open class AttributedLabel: UIView {
             }
         }
     }
-    
+
     /// If need to use intrinsicContentSize set true. Also should call invalidateIntrinsicContentSize when intrinsicContentSize is cached. When text was changed for example.
     public var intrinsicAutoLayout = false
-    
+
     var mergedAttributedText: NSAttributedString? {
         if let attributedText = attributedText {
             return mergeAttributes(attributedText)
         }
         return nil
     }
-    
+
     let container = NSTextContainer()
     let layoutManager = NSLayoutManager()
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         isOpaque = false
         contentMode = .redraw
         lineBreakMode = .byTruncatingTail
         padding = 0
         layoutManager.addTextContainer(container)
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
+
         isOpaque = false
         contentMode = .redraw
         lineBreakMode = .byTruncatingTail
         padding = 0
         layoutManager.addTextContainer(container)
     }
-    
+
     open override func setNeedsDisplay() {
         if Thread.isMainThread {
             super.setNeedsDisplay()
         }
     }
-    
+
     open override var intrinsicContentSize: CGSize {
         if intrinsicAutoLayout {
             guard let attributedText = mergedAttributedText else {
@@ -166,60 +166,60 @@ open class AttributedLabel: UIView {
             return bounds.size
         }
     }
-    
+
     open override func draw(_ rect: CGRect) {
         guard let attributedText = mergedAttributedText else {
             return
         }
-        
+
         let storage = NSTextStorage(attributedString: attributedText)
         storage.addLayoutManager(layoutManager)
-        
+
         container.size = rect.size
         let frame = layoutManager.usedRect(for: container)
         let point = contentAlignment.alignOffset(viewSize: rect.size, containerSize: frame.integral.size)
-        
+
         let glyphRange = layoutManager.glyphRange(for: container)
         layoutManager.drawBackground(forGlyphRange: glyphRange, at: point)
         layoutManager.drawGlyphs(forGlyphRange: glyphRange, at: point)
     }
-    
+
     open override func sizeThatFits(_ size: CGSize) -> CGSize {
         guard let attributedText = mergedAttributedText else {
             return .zero
         }
-        
+
         let storage = NSTextStorage(attributedString: attributedText)
         storage.addLayoutManager(layoutManager)
-        
+
         container.size = size
         let frame = layoutManager.usedRect(for: container)
         return frame.integral.size
     }
-    
+
     open override func sizeToFit() {
         super.sizeToFit()
-        
+
         frame.size = sizeThatFits(CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude))
     }
-    
+
     private func mergeAttributes(_ attributedText: NSAttributedString) -> NSAttributedString {
         let attrString = NSMutableAttributedString(attributedString: attributedText)
-        
 
         addAttribute(attrString, attrName: .font, attr: font)
+
         if let textColor = textColor {
             addAttribute(attrString, attrName: .foregroundColor, attr: textColor)
         }
-        
+
         if let paragraphStyle = paragraphStyle {
             addAttribute(attrString, attrName: .paragraphStyle, attr: paragraphStyle)
         }
-        
+
         if let shadow = shadow {
             addAttribute(attrString, attrName: .shadow, attr: shadow)
         }
-        
+
         return attrString
     }
 
@@ -232,3 +232,4 @@ open class AttributedLabel: UIView {
         }
     }
 }
+
